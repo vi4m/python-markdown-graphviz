c031c7d464ff1d7670832b20445ed92cfb142cfd
Johan Forsberg <johan.forsberg@edgeware.tv>
2013-09-24 16:38:01 +0200

    add plantuml plugin to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='python-markdown-graphviz',
     version='0.1',
-    py_modules=['mdx_graphviz', 'mdx_ditaa'],
+    py_modules=['mdx_graphviz', 'mdx_ditaa', 'mdx_plantuml'],
 )
