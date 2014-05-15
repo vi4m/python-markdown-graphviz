0781e8e62b0f525d70a79931f67fe699b30b238a
Par Tojkander <par.tojkander@edgeware.tv>
2014-05-15 09:20:08 +0200

    Add mdx_boldcode.py to setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 
 setup(
     name='python-markdown-graphviz',
-    version='0.1',
-    py_modules=['mdx_graphviz', 'mdx_ditaa', 'mdx_plantuml'],
+    version='0.2',
+    py_modules=['mdx_graphviz', 'mdx_ditaa', 'mdx_plantuml', 'mdx_boldcode.py'],
 )
