100a39389151ec7df674eece001d8c3d216e9fd6
Jesper Brown <jesper.brown@gmail.com>
2014-05-15 17:14:21 +0200

    Removed error in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(
     name='python-markdown-graphviz',
     version='0.2',
-    py_modules=['mdx_graphviz', 'mdx_ditaa', 'mdx_plantuml', 'mdx_boldcode.py'],
+    py_modules=['mdx_graphviz', 'mdx_ditaa', 'mdx_plantuml', 'mdx_boldcode'],
 )
